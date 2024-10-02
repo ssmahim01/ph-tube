2ec7b5bbc98b464e8b0063cf334c820c9c8ea263
ssmahim01 <mahimzayn666@gmail.com>
2024-10-03 02:13:04 +0600

    Edited Script file

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/* Get string of time */
+
+function getTimeString(time){
+   const hour = parseInt(time / 3600);
+   let remainingSec = time % 3600;
+
+   const minute = parseInt(remainingSec / 60);
+   remainingSec = remainingSec % 60;
+   return `${hour} hour ${minute} minute ${remainingSec} second ago`; 
+};
+
 /* Create Load Categories */
 
 const loadCategories = () => {
@@ -14,23 +25,33 @@ const loadVideos = () => {
    .catch(error => console.log(error))
 };
 
+const loadCategoryVideos = (id) => {
+   fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+   .then(response => response.json())
+   .then(data => displayVideos(data.category))
+   .catch(error => console.log(error))
+};
+
 /* Create Display Categories */
 
 const displayCategories = (categories) => {
-
     const categoryContainer = document.getElementById('category-container');
 
     categories.forEach((item) => {
        const button = document.createElement('button');
        button.classList = 'btn';
        button.innerText = item.category;
-
-       categoryContainer.append(button);
+      const div = document.createElement('div');
+      div.innerHTML = `
+      <button onclick="loadCategoryVideos(${item.category_id})" class="btn">${item.category}</button>
+      `;
+       categoryContainer.append(div);
     });
 };
 
 const displayVideos = (videos) => {
     const videosContainer = document.getElementById('videos');
+    videosContainer.innerHTML = "";
 
     videos.forEach((video) => {
        const card = document.createElement('div');
@@ -39,7 +60,7 @@ const displayVideos = (videos) => {
         <figure class="h-[200px] relative">
         <img class="w-full h-full object-cover"
         src=${video.thumbnail} />
-        ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black rounded text-white p-1">${video.others.posted_date}</span>`}
+        ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black rounded text-white text-xs p-1">${getTimeString(video.others.posted_date)}</span>`}
         </figure>
         <div class="flex gap-2 px-0 py-2">
         <div>
