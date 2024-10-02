3ec8690c3a43b68ea161b40f92fb7cef01cee5f8
ssmahim01 <mahimzayn666@gmail.com>
2024-10-03 01:08:05 +0600

    Added HTML and JS file with Tailwind file

---
+++ script.js
@@ -0,0 +1,64 @@
+/* Create Load Categories */
+
+const loadCategories = () => {
+   fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+   .then(response => response.json())
+   .then(data => displayCategories(data.categories))
+   .catch(error => console.log(error))
+};
+
+const loadVideos = () => {
+   fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+   .then(response => response.json())
+   .then(data => displayVideos(data.videos))
+   .catch(error => console.log(error))
+};
+
+/* Create Display Categories */
+
+const displayCategories = (categories) => {
+
+    const categoryContainer = document.getElementById('category-container');
+
+    categories.forEach((item) => {
+       const button = document.createElement('button');
+       button.classList = 'btn';
+       button.innerText = item.category;
+
+       categoryContainer.append(button);
+    });
+};
+
+const displayVideos = (videos) => {
+    const videosContainer = document.getElementById('videos');
+
+    videos.forEach((video) => {
+       const card = document.createElement('div');
+       card.classList = "card card-compact";
+       card.innerHTML = `
+        <figure class="h-[200px] relative">
+        <img class="w-full h-full object-cover"
+        src=${video.thumbnail} />
+        ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black rounded text-white p-1">${video.others.posted_date}</span>`}
+        </figure>
+        <div class="flex gap-2 px-0 py-2">
+        <div>
+        <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+        </div>
+        <div>
+        <h2 class="font-bold">${video.title}</h2>
+        <div class="flex items-center gap-2">
+        <p class="text-gray-400 font-medium">${video.authors[0].profile_name}</p>
+        ${video.authors[0].verified == true? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>`:""}
+        </div>
+        </div>
+        </div>
+     </div>
+       `
+
+       videosContainer.append(card);
+    });
+}
+
+loadCategories();
+loadVideos();
